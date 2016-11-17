5dc692a0f75553e8d0b495cb463486a8f42ba610
maps_red <maps_red@yahoo.fr>
2016-11-17 11:03:15 +0100

    Add UserBundle content

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -4,14 +4,47 @@ namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/login", name="security_login")
+     * @return Response
      */
-    public function indexAction()
+    public function loginAction()
     {
-        return $this->render('UserBundle:Default:index.html.twig');
+        if ($this->getUser()) {
+            return $this->redirectToRoute('homepage');
+        }
+        $helper = $this->get('security.authentication_utils');
+        $exception = $this->get('security.authentication_utils')->getLastAuthenticationError();
+        $twigArray['last_username'] = $helper->getLastUsername();
+        $twigArray['error'] = $exception ? $exception : null;
+
+        return $this->render('UserBundle:Security:login.html.twig', $twigArray);
+    }
+
+    /**
+     * @Route("/login_check", name="security_login_check")
+     */
+    public function loginCheckAction()
+    {
+        // will never be executed
+    }
+
+    /**
+     * @Route("/register", name="security_register")
+     */
+    public function RegisterAction()
+    {
+    }
+
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function profileAction()
+    {
+
     }
 }

dc7b9e4e33a0b9857685003f154049cd0c6e9372
maps_red <maps_red@yahoo.fr>
2016-11-17 11:26:24 +0100

    Add User Entity and connection system

---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,246 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Knp\DoctrineBehaviors\Model as ORMBehaviors;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\UserRepository")
+ */
+class User implements UserInterface
+{
+    use ORMBehaviors\Timestampable\Timestampable;
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="uuid", type="string", length=255, unique=true)
+     */
+    private $uuid;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="api_token", type="string", length=255, unique=true)
+     */
+    private $apiToken;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="roles", type="json_array")
+     */
+    private $roles;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="enabled", type="boolean", options={"default" : 1})
+     */
+    private $enabled = 1;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set uuid
+     *
+     * @param string $uuid
+     *
+     * @return User
+     */
+    public function setUuid($uuid)
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    /**
+     * Get uuid
+     *
+     * @return string
+     */
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set apiToken
+     *
+     * @param string $apiToken
+     *
+     * @return User
+     */
+    public function setApiToken($apiToken)
+    {
+        $this->apiToken = $apiToken;
+
+        return $this;
+    }
+
+    /**
+     * Get apiToken
+     *
+     * @return string
+     */
+    public function getApiToken()
+    {
+        return $this->apiToken;
+    }
+
+    /**
+     * Set roles
+     *
+     * @param array $roles
+     *
+     * @return User
+     */
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * Get roles
+     *
+     * @return array
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        return;
+    }
+
+    /**
+     * Get enabled
+     *
+     * @return boolean
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Set enabled
+     *
+     * @param boolean $enabled
+     *
+     * @return User
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+}
+
---
+++ src/UserBundle/Security/CustomAuthenticator.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maps_red
+ * Date: 27/07/2016
+ * Time: 23:22
+ */
+
+namespace UserBundle\Security;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use UserBundle\Entity\User;
+use Symfony\Component\Security\Core\Security;
+
+/**
+ * Class CustomAuthenticator
+ * @package UserBundle\Security
+ */
+class CustomAuthenticator extends AbstractGuardAuthenticator
+{
+    const BANNED = "Votre compte a été désactivé, veuillez contacter un administrateur afin d'obtenir plus de renseignements";
+    /** @var EntityManager $em */
+    private $em;
+    /** @var ContainerInterface $container */
+    private $container;
+
+
+    /**
+     * CustomAuthenticator constructor.
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->em = $container->get("doctrine")->getManager();
+        $this->container = $container;
+    }
+
+    /**
+     * step 1
+     * Called on every request. Return whatever credentials you want,
+     * or null to stop authentication.
+     * @param Request $request
+     * @return array
+     */
+    public function getCredentials(Request $request)
+    {
+        if ($request->getPathInfo() != '/login_check') {
+            return null;
+        }
+
+        $username = ucfirst(trim($request->request->get('_username')));
+        $password = $request->request->get('_password');
+
+        return ['username' => $username, 'password' => $password];
+    }
+
+    /**
+     * step 2
+     * @param mixed $credentials
+     * @param UserProviderInterface $userProvider
+     * @return bool|UserInterface
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $user = $this->em->getRepository('UserBundle:User')->findByUsernameOrUUID($credentials['username']);
+        if (!$user) {
+            return false;
+        }
+
+        return $user;
+    }
+
+    /**
+     * step 3
+     * @param mixed $credentials
+     * @param UserInterface|User $user
+     * @return bool
+     */
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        $encoder = $this->container->get('security.password_encoder');
+        if (!$user->getEnabled()) {
+            throw new CustomUserMessageAuthenticationException(self::BANNED);
+        }
+
+        return $encoder->isPasswordValid($user, $credentials['password']);
+    }
+
+    /**
+     * @param Request $request
+     * @param TokenInterface $token
+     * @param string $providerKey
+     * @return RedirectResponse
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return null;
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthenticationException $exception
+     * @return RedirectResponse
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+
+        return new RedirectResponse($this->container->get('router')->generate('security_login'));
+    }
+
+    /**
+     * Called when authentication is needed, but it's not sent
+     * @param Request $request
+     * @param AuthenticationException|null $authException
+     * @return JsonResponse
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        return new JsonResponse(['message' => 'Connexion requise'], 401);
+    }
+
+    /**
+     * @return bool
+     */
+    public function supportsRememberMe()
+    {
+        return false;
+    }
+}

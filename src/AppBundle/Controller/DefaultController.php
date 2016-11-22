8a95a94ad23656cd1ab26e815b308a4b9fa37fdd
maps_red <maps_red@yahoo.fr>
2016-11-22 12:56:49 +0100

    Add common route for navigo uuid

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,33 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @return Response
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
+    }
+
+
+    /**
+     * @Route("/card/{uuid}", name="card_route")
+     * @param null $uuid
+     */
+    public function checkValidAction($uuid = null)
+    {
+        if ($uuid) {
+            $card = $this->getDoctrine()->getRepository("UserBundle:Card")->findOneBy(['uuid' => $uuid]);
+        }
+
+
     }
 }
